3f02a2f835054bd1860ee497d9285e5964d04d40
Most <foremost545@gmail.com>
2022-11-02 00:23:28 +0700

    Add composable TopAppBar

--- app/src/main/java/com/most/oneapp/HomeScreen.kt
+++ app/src/main/java/com/most/oneapp/HomeScreen.kt
@@ -1,23 +1,12 @@
 package com.most.oneapp
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.ChatBubble
-import androidx.compose.material.icons.rounded.Search
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
-import com.most.oneapp.ui.theme.ButtonGray
+import com.most.oneapp.composable.TopAppBar
 
 @Composable
 fun HomeScreen() {
@@ -26,34 +15,6 @@ fun HomeScreen() {
             .background(MaterialTheme.colorScheme.background)
             .fillMaxWidth()
     ) {
-
-        Row(
-            Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 8.dp, vertical = 8.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Text(text = stringResource(R.string.app_name), style = MaterialTheme.typography.titleMedium)
-            Spacer(Modifier.weight(1f))
-            // icon
-            IconButton(
-                onClick = {},
-                modifier = Modifier
-                    .clip(CircleShape)
-                    .background(ButtonGray)
-            ) {
-                Icon(
-                    Icons.Rounded.Search,
-                    contentDescription = stringResource(id = R.string.search)
-                )
-            }
-            Spacer(modifier = Modifier.width(8.dp))
-            IconButton(onClick = {},
-                modifier = Modifier
-                    .clip(CircleShape)
-                    .background(ButtonGray)) {
-                Icon(imageVector = Icons.Rounded.ChatBubble, stringResource(id = R.string.search))
-            }
-        }
+        TopAppBar("Hello Most", onClickSearch = {}, onClickMessage = {})
     }
 }
